c87c0e80c7154fe46c1576d53ccced05700ec487
novarizky <novasumbawa20@gmail.com>
2022-06-18 16:53:15 +0800

    test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV != "production") require("dotenv").config();
 const express = require('express')
 const router = require('./router')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4200
 const {run, getDb} = require('./config/mongoDB')
 const cors = require('cors')
 
